c55963d5bea417fd0eb1ec0673f6bfad5fdf3bb3
LeksoBugadze <166960874+LeksoBugadze@users.noreply.github.com>
2024-06-11 03:32:22 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const displayResult=document.getElementById("result");
 const displayModal=document.getElementById("modal");
+const displayHeads=document.getElementById("head");
+const displayTails=document.getElementById("tails");
 
+
+
+const score=JSON.parse(localStorage.getItem('score'))||{
+    heads:0,
+    tails:0
+};
+
+displayHeads.textContent=score.heads;
+displayTails.textContent=score.tails;
 
 function appendToDisplay(){
     displayModal.style.display='flex';
     const randomNumber=Math.random();
     const result=flip(randomNumber);
     displayResult.textContent=''+result;
+    if(result==='Heads'){
+        score.heads++
+    }else score.tails++;
+    
+    displayHeads.textContent=score.heads;
+    displayTails.textContent=score.tails;
+    localStorage.setItem('score',JSON.stringify(score));
+    
 }
 
 function flip(randomNumber){
@@ -21,3 +40,9 @@ function hideModal(){
     displayModal.style.display='none';
 }
 
+function reset(){
+    score.heads=0;
+    score.tails=0;
+    displayHeads.textContent=score.heads;
+    displayTails.textContent=score.tails;
+}
